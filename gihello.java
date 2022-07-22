8014c484ecfdb81c0212fb9c973c4f06049755ad
Komala <komalasaravanan84@gmail.com>
2022-07-22 11:03:16 +0200

    added values of a,b,c

--- gihello.java
+++ gihello.java
@@ -2,6 +2,10 @@ public class gihello {
     public static void main(String[] arg) {
         System.out.println("Hello Bonjour!");
         System.out.println("Hello world");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("the value of c" + c);
 
     }
 }
