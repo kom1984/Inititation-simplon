2af35ca513d1589a0955c9f8082d64f04e58032c
Komala <komalasaravanan84@gmail.com>
2022-07-22 10:48:30 +0200

    changes done

--- gihello.java
+++ gihello.java
@@ -1,6 +1,7 @@
-public class gihello{
-    public static void main(String[] arg)
-{
-System.out.println("Hello Bonjour!");
-}
+public class gihello {
+    public static void main(String[] arg) {
+        System.out.println("Hello Bonjour!");
+        System.out.println("Hello world");
+
+    }
 }
